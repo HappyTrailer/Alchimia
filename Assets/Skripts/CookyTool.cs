8e0da5e3b513c862b8ca30108d74b9de84bf8953
HappyTrailer <285.zubchenko@gmail.com>
2017-01-27 17:41:29 +0200

    Drag Tool

---
+++ Assets/Skripts/CookyTool.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class CookyTool : MonoBehaviour, IDragHandler, /*IDropHandler,*/ IPointerDownHandler, IPointerUpHandler{
+
+    Vector3 startPos;
+
+	void Start () {
+        startPos = transform.position;
+	}
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    /*public void OnDrop(PointerEventData eventData)
+    {
+        transform.position = startPos;
+    }*/
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        transform.position = startPos;
+    }
+}
