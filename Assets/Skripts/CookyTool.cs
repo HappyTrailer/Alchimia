4a98d1c4577a8357e8b924b02d6bce30612dc1ff
HappyTrailer <285.zubchenko@gmail.com>
2017-01-28 23:23:20 +0200

    Drag Tool Calculating 90%

--- Assets/Skripts/CookyTool.cs
+++ Assets/Skripts/CookyTool.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class CookyTool : MonoBehaviour, IDragHandler, /*IDropHandler,*/ IPointerDownHandler, IPointerUpHandler{
+    
+    public Text text;
 
     Vector3 startPos;
 
@@ -12,6 +15,22 @@ public class CookyTool : MonoBehaviour, IDragHandler, /*IDropHandler,*/ IPointer
 
     public void OnDrag(PointerEventData eventData)
     {
+        float x = 0;
+        float y = 0;
+
+        if (transform.position.x > eventData.position.x)
+            x = transform.position.x - eventData.position.x;
+        else
+            x = eventData.position.x - transform.position.x;
+        if (transform.position.y > eventData.position.y)
+            y = transform.position.y - eventData.position.y;
+        else
+            y = eventData.position.y - transform.position.y;
+
+        float number = System.Convert.ToSingle(text.text);
+        number -= (x + y);
+        text.text = number.ToString("F0");
+
         transform.position = eventData.position;
     }
 
