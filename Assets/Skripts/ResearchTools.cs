9404d93030e80706e5c2dcecf10696fbd6b46e55
lKORENl <korenanon@gmail.com>
2017-01-28 06:52:54 +0200

    Script work

---
+++ Assets/Skripts/ResearchTools.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResearchTools : MonoBehaviour {
+    //в классе информация о исследовательских инструментах
+    // и методы для просчета вероятностей открытия рецептов
+    int mortar1 = 1;
+    int distiller = 1;
+    int Blending = 1;
+
+    
+}
