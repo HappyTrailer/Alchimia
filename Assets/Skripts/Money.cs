72e75c26394951adda39d9ad5a07d9366386d1c2
HappyTrailer <285.zubchenko@gmail.com>
2017-02-15 22:24:19 +0200

    Shop 50%

---
+++ Assets/Skripts/Money.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Money : MonoBehaviour 
+{
+    public static float money;
+
+    void Start()
+    {
+        money = 500;
+    }
+}
---
+++ Assets/Skripts/TradeIngridients.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class TradeIngridients : MonoBehaviour
+{
+    public GameObject container;
+    public GameObject itemsPanel;
+    public GameObject money;
+    public GameObject txt;
+
+    GameObject item;
+    int randomIngr;
+    float timer;
+
+    void Start()
+    {
+        timer = 600;
+        FillShop();
+	}
+
+    void Update()
+    {
+        if (timer >= 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            FillShop();
+            timer = 600;
+        }
+        txt.GetComponent<Text>().text = ((int)(timer / 60)).ToString("D2") + ":" + ((int)(timer % 60)).ToString("D2");
+        money.GetComponent<Text>().text = Money.money.ToString();
+    }
+
+    public void FillShop()
+    {
+        container.SetActive(true);
+        randomIngr = Random.Range(4, 10);
+        for (int i = 0; i < itemsPanel.transform.childCount; i++)
+        {
+            if (itemsPanel.transform.GetChild(i).name != "Container")
+                Destroy(itemsPanel.transform.GetChild(i).gameObject);
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            int temp = i;
+            item = Instantiate(container);
+            item.transform.name = "Item";
+            item.transform.parent = itemsPanel.transform;
+            item.transform.localScale = new Vector3(1, 1, 1);
+            item.transform.GetChild(0).GetComponent<Image>().sprite = Resources.Load<Sprite>(ListIngredients.masIngredient[i].Sprite);
+            item.transform.GetChild(1).GetComponent<Text>().text = ListIngredients.masIngredient[i].Name;
+            item.transform.GetChild(2).GetComponent<Button>().onClick.AddListener(() => { BuyIngtidient(temp); });
+        }
+        int[] mass = new int[randomIngr];
+        int id = 0;
+        for (int i = 0; i < randomIngr; i++)
+		{
+            do
+            {
+                id = Random.Range(3, ListIngredients.masIngredient.Length);
+                if (!mass.Contains(id))
+                {
+                    mass[i] = id;
+                    int temp = id;
+                    item = Instantiate(container);
+                    item.transform.parent = itemsPanel.transform;
+                    item.transform.localScale = new Vector3(1, 1, 1);
+                    item.transform.GetChild(0).GetComponent<Image>().sprite = Resources.Load<Sprite>(ListIngredients.masIngredient[id].Sprite);
+                    item.transform.GetChild(1).GetComponent<Text>().text = ListIngredients.masIngredient[id].Name;
+                    item.transform.GetChild(2).GetComponent<Button>().onClick.AddListener(() => { BuyIngtidient(temp); });
+                }
+            } while (mass[i] != id);
+        }
+        container.SetActive(false);
+    }
+
+    void BuyIngtidient(int id)
+    {
+        Debug.Log(id);
+    }
+}
