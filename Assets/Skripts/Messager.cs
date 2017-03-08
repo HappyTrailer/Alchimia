03078aa0b2b79d0d7bca7cb4be2c5f2a29f1532a
HappyTrailer <285.zubchenko@gmail.com>
2017-03-08 20:31:49 +0200

    Chek commit

--- Assets/Skripts/Messager.cs
+++ Assets/Skripts/Messager.cs
@@ -2,15 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Messager : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class Messager : MonoBehaviour 
+{
+    
 }
