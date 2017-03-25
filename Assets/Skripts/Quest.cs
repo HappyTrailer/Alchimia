c17730d9870815d81c580f5d5075e4baad272e66
lKORENl <korenanon@gmail.com>
2017-03-25 16:50:16 +0200

    Quest work start

---
+++ Assets/Skripts/Quest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Класс опичывает задание
+public class Quest : MonoBehaviour {
+
+    string name; //название
+
+    int money;   //награда
+    int timeLimit; //ограничение по времени
+    //int countRepeat; //количество повторений
+   // int currentRepeat; // текущее повторение
+
+    int[,] masPotion; //масив для записи - id зелья | количество повторов | выполнено повторов
+    
+    public Quest(string name,
+                int money, 
+                int timeLimit, 
+                //int countRepeat,
+                //int currentRepeat,
+                int[,] masPotion)
+    {
+        this.name = name;
+
+        this.money = money;
+        this.timeLimit = timeLimit;
+        //this.countRepeat = countRepeat;
+        //this.currentRepeat = currentRepeat;
+
+        this.masPotion = masPotion;
+    }
+
+}
---
+++ Assets/Skripts/QuestGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestGenerator : MonoBehaviour {
+
+	//класс отвечает за отображение новых заданий.
+    //генерацию случайных заданий
+    //появление заданий разных уровней сложности
+    //проверки по наличию необходимых рецептов или герерация рандомного рецепта с разной сложностью
+}
