9e5e2870a04aa18b02dc2ca26fb3e17dd141a92d
lKORENl <korenanon@gmail.com>
2017-04-28 20:55:21 +0300

    ww

--- Assets/Skripts/QuestGenerator.cs
+++ Assets/Skripts/QuestGenerator.cs
@@ -29,19 +29,24 @@ public class QuestGenerator : MonoBehaviour
     {
         //Ограничение на рандомный рецепт от количества открытых ингредиентов
         for (int j = 1; j < currentGarade; j++)
-            RandFirstTypeQuest(RandCountQuest(80, 4), j); // генерация заданий первого типа
+            //RandFirstTypeQuest(RandCountQuest(80, 4), j); // генерация заданий первого типа
 
-        //RandSecondTypeQuest(RandCountQuest(50, 3)); // генерация заданий второго типа 
+        RandSecondTypeQuest(RandCountQuest(50, 3),j); // генерация заданий второго типа 
 
         //RandThirdTypeQuest(RandCountQuest(15, 2)); // генерация заданий третьего типа
         Debug.Log(randQuestList.Count);
-
-        for (int i = 0; i < randQuestList.Count; i++)
+        if (randQuestList.Count != 0)
         {
-            Debug.Log(randQuestList[i].nameQuest + " id = " + randQuestList[i].masPotion[0].id);
+            Debug.Log(randQuestList[0].masPotion.Length);
+            for (int i = 0; i < randQuestList.Count; i++)
+            {
+                Debug.Log(randQuestList[i].nameQuest);
+                for (int j = 0; j < randQuestList[i].masPotion.Length; j++)
+                    Debug.Log(" id = " + randQuestList[i].masPotion[j].id);
+                Debug.Log("====================================");
+            }
+            Debug.Log("---------------------------");
         }
-        Debug.Log("---------------------------");
-
     }
 
 
@@ -87,27 +92,44 @@ public class QuestGenerator : MonoBehaviour
         if (list.Count > 2)
         {
             Quest.StrQ[] qmass = CreateMasStructQust(list);
-            for (int i = 0; i < countQuest; i++)
-            {
-                int r = Random.Range(0, list.Count); //получение рандомного рецепта из списка доступных
-                int countRepit = Random.Range(1, 3); //количество повторений
-                if (randQuestList.Count == 0 || list[r] != randQuestList[randQuestList.Count - 1].masPotion[0].id)
-                {
-                    Quest q = new Quest("garde " + ListRecipePotion.masRecPotion[list[r]] + " first type №" + i.ToString(), 1 * i, RandTimeLimit(), new Quest.StrQ[] { new Quest.StrQ(list[r], countRepit, 0) });//создание отдельного квеста
 
+            for (int i = 0; i < countQuest; i++)
+            {                       
+                    Quest q = new Quest("garde = " + grade + " second type №" + i.ToString(), 1 * i, RandTimeLimit(), qmass);//создание отдельного квеста
                     randQuestList.Add(q); //Добавление квеста в список доступных
-                    timeChance += 10; //увеличение шанса на появление временного ограничения     
-                }
+                    timeChance += 10; //увеличение шанса на появление временного ограничения                    
             }
         }
 
         timeChance = 10;
     }
+
     Quest.StrQ[] CreateMasStructQust(List<int> list)
     {
-        int r = Random.Range(0, list.Count);
+        int r = Random.Range(2, list.Count); // количество зелий в задании
+        List<int> usMass = new List<int>();
 
-        return new Quest.StrQ[1];
+            Quest.StrQ[] q = new Quest.StrQ[r];
+
+        
+
+        for (int i = 0; i < r; i++)
+        {           
+            int rec = Random.Range(0, list.Count); //получение рандомного рецепта из списка доступных
+            while (!usMass.Contains(rec))
+            {
+                rec = Random.Range(0, list.Count);
+            }
+
+            int countRepit = Random.Range(1, 5); //количество повторений
+            q[i] = new Quest.StrQ(list[rec], countRepit, 0);
+            usMass.Add( list[rec]);
+
+        }
+
+   
+
+        return q;
     }
 
     void RandThirdTypeQuest(int countQuest, int grade)//Метод для создания заданий третьего типа(с рандомным рецептом) и добавление их в общий список
